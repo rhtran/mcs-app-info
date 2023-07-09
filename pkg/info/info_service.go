6fbed60194a7644e829fe35204c34fb10a8badd6
Ryan Tran <66757107+ryyt-cg@users.noreply.github.com>
2023-07-09 08:14:31 -0700

    Update info_service.go

--- pkg/info/info_service.go
+++ pkg/info/info_service.go
@@ -12,20 +12,13 @@ type Servicer interface {
 type Service struct {
 	logger     *zap.Logger
 	commitHash string
+	info Info
 }
 
-func NewInfoService(logger *zap.Logger, commitHash string) *Service {
-	return &Service{logger, commitHash}
+func NewInfoService(logger *zap.Logger, commitHash string, info Info) *Service {
+	return &Service{logger, commitHash, info}
 }
 
 func (service *Service) GetAppInfo() (*Info, error) {
-	// this block make GetAppInfo() not testable.
-	info := &Info{
-		AppName:     app.Config.AppInfo.Name,
-		Description: app.Config.AppInfo.Description,
-		GitCommit:   service.commitHash,
-		Version:     service.commitHash,
-	}
-
-	return info, nil
+	return &service.info, nil
 }
